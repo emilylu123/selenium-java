08fd4d93047997d9e86ab5eedcdba0c3fc53a9f9
Yingyao Lu <emilylu123@gmail.com>
2021-04-11 18:06:04 +0930

    test with xpath

--- src/main/java/EmilySeleniumTest.java
+++ src/main/java/EmilySeleniumTest.java
@@ -1,3 +1,5 @@
+import static org.junit.Assert.assertTrue;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Before;
@@ -7,11 +9,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class EmilySeleniumTest {
 	private WebDriver driver;
+	private WebElement element;
 
 	// Use WebDriverManager Maven package to download the correct version of a driver to automate
 	// Chrome browser. This method will run once before all of the tests in our class
@@ -20,13 +21,13 @@ public class EmilySeleniumTest {
 		WebDriverManager.chromedriver().setup();
 	}
 
-	//Set variable driver to an instance of ChromeDriver
+	// 1. Initialise ChromeDriver
 	@Before
 	public void setupTest() {
 		driver = new ChromeDriver();
 	}
 
-	// quit session
+	// 7. quit session
 	@After
 	public void teardown() {
 		if (driver != null) {
@@ -35,37 +36,59 @@ public class EmilySeleniumTest {
 	}
 
 	@Test
-	public void emilyProfileTest(){
+	public void emilyPortfolioTest(){
 		// 2. Navigation
 		// driver.get("http://yingyao.codes/");
 		driver.navigate().to("https://emilylu123.github.io");
 
-		// 3. Find element
-		// 4. check the state
-		// wait 10 seconds
-		WebDriverWait wait = new WebDriverWait(driver,10);
-		WebElement githubIcon = wait.until(ExpectedConditions.elementToBeClickable(By.className(
-				"fa-github")));
-		// 5. take action
-		// 6. record the result
-		// test github link and go back
-		githubIcon.click();
-		driver.navigate().back();
+		// Test Github Link
+		testLink();
 
 		// Test contact form
-		// fill in email and message and click submit btn
-		WebElement inputEmail = driver.findElement(By.id("email"));
-		inputEmail.sendKeys("emilylu@gmail.com");
+		testContact();
+
+		// Test thanks pages
+		testThanksPage();
+	}
+
+	@Test
+	// test Github link and go back
+	public void testLink(){
+		// 3. Find element
+		element = driver.findElement(By.className("fa-github"));
+		// 4. check the state
+		assertTrue(element.isDisplayed());
+		// 5. take action
+		element.click();
+		driver.navigate().back();
+	}
+
+	@Test
+	// Fill in email and message and click submit btn
+	public void testContact(){
+		// Find, restore and act
+		WebElement inputEmail = driver.findElement(By.id("contact-email"));
+		inputEmail.sendKeys("test@gmail.com");
 
 		WebElement inputMessage = driver.findElement(By.name("message"));
-		inputMessage.sendKeys("Test Test Test");
+		inputMessage.sendKeys("Test: " + (int)(Math.random()*1000));
 
-		WebElement btnSubmit = driver.findElement(By.className("btn-primary"));
+		// submit contact form
+		WebElement btnSubmit = driver.findElement(By.id("btn-submit-form"));
 		btnSubmit.submit();
+	}
 
-		// assertTrue(githubIcon.isDisplayed());
-		// System.out.println(title);
-		// 7. quit session
-		// driver.quit();
+	@Test
+	// test confirmation page
+	public void testThanksPage(){
+		// driver.get("https://formspree.io/thanks?language=zh");
+		// select return message in the forwarded page
+		element = driver.findElement(By.xpath("//h1/following-sibling::p"));
+		System.out.println(element.toString());
+
+		// check successful message
+		assertTrue(element.getText().contains("successful"));
+		// 6. record the result
+		System.out.println(element.getText());
 	}
 }
